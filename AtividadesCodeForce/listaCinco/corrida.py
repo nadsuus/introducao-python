5f1d8842a5982c5510d0d5f5f415e278af6e99a0
Nadsuus <nadsontvw@gmail.com>
2024-12-05 14:33:53 -0300

    corrida lista5

---
+++ AtividadesCodeForce/listaCinco/corrida.py
@@ -0,0 +1,10 @@
+# Numero da charete, distancia(metros), velocidade(km)
+charrete1 = list(input().split())
+charrete2 = list(input().split())
+
+tcha1 = charrete1[2]/charrete1[1]
+tcha2 = charrete1[2]/charrete1[1]
+
+s = charrete1[1]-
+charrete1[1]*charrete1[2]
+
---
+++ AtividadesCodeForce/listaQuatro/notaCortada.py
@@ -0,0 +1,11 @@
+B = int(input())
+T = int(input())
+
+soma = B + T
+
+if soma > 160:
+    print(1)
+elif soma < 160:
+    print(2) 
+else:
+    print(0)  
