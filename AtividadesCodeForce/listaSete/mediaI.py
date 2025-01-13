7ae45b65fffe20e8a53e95bb59cbbab3c7e290b0
Nadsuus <nadsontyw@gmail.com>
2025-01-13 16:01:51 -0300

    gg

--- AtividadesCodeForce/listaSete/mediaI.py
+++ AtividadesCodeForce/listaSete/mediaI.py
@@ -1,3 +1,13 @@
 n = int(input())
-for i in range(0,n):
-    nlista = ma
+nlista = list(map(int,input().split()))
+media = sum(nlista) / n
+naMedia = 0
+abaixoMedia = 0
+for i in nlista:
+    if i < media:
+        abaixoMedia += 1
+    elif i >= media:
+        naMedia += 1
+print(""f"{media:.1f}")
+print(abaixoMedia)
+print(naMedia)
