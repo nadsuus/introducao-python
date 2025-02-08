5067c55ac42371f5d48c3f159e478d09a8c10b10
nsantos <nadson.santos.nmt@gmail.com>
2025-02-08 17:37:36 -0300

    gg

---
+++ AtividadesCodeForce/Lista_9/Achando monótonos não triviais maximais.py
@@ -0,0 +1,6 @@
+n = int(input())
+x = str(input())
+
+for i in range(len(x)):
+    if x[i] == x[i+1]:
+        
--- AtividadesCodeForce/Lista_9/enigma.py
+++ AtividadesCodeForce/Lista_9/enigma.py
@@ -1,3 +1,18 @@
-mensagemCifrada = str(input())
-crib = str(input())
+mensagem = input().strip()
+crib = input().strip()
 
+tamanhoMensagem = len(mensagem)
+tamanhoCrib = len(crib)
+
+posicoesValidas = 0
+
+for i in range(tamanhoMensagem - tamanhoCrib + 1):
+    valido = True
+    for j in range(tamanhoCrib):
+        if mensagem[i + j] == crib[j]:
+            valido = False
+            break
+    if valido:
+        posicoesValidas += 1
+        
+print(posicoesValidas)
